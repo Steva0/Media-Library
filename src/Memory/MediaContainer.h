0212cafe94f1780d0c683b2332f24664bdd03620
b0dyless <almor.social@proton.me>
2025-05-19 10:57:50 +0200

    aggiunta interfaccia MediaContainer

---
+++ src/Memory/MediaContainer.h
@@ -0,0 +1,13 @@
+#ifndef MEMORY_MEDIA_CONTAINER_H
+#define MEMORY_MEDIA_CONTAINER_H
+#include "../Media/Media.h"
+namespace memory {
+class MediaContainer {
+ public:
+  MediaContainer() = default;
+  int addMedia(media::Media *);
+  int removeMedia(media::Media *);
+  std::vector<media::Media *> filter(media::Media *);
+};
+}  // namespace memory
+#endif
