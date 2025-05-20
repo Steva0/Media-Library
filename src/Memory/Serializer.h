15db58b665a3a7ab592637732895462555ddc1c0
Steva0 <michele.stevanin@gmail.com>
2025-05-20 16:12:29 +0200

    Sistemati nomi file

--- src/Memory/Serializer.h
+++ src/Memory/Serializer.h
@@ -1,17 +1,16 @@
 #ifndef MEMORY_SERIALIZER_H
 #define MEMORY_SERIALIZER_H
 #include <QFile>
-
 #include "../Media/Media.h"
 
 namespace memory {
 class Serializer {
  private:
-  static std::vector<media::Media *>& XMLtoVec(const QFile &);
-  static std::vector<media::Media *>& JSONtoVec(const QFile &);
+  static int vecToJSON(const std::vector<media::Media *> &, QFile &);
+  static int vecToXML(const std::vector<media::Media *> &, QFile &);
 
  public:
-  static std::vector<media::Media *>& serialize(const QFile &);
+  static int Serialize(const std::vector<media::Media *> &, QFile &);
 };
 }  // namespace memory
 #endif
