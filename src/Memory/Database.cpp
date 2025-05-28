c802534c87fd0c4ae1b7f7d0b5d1e2b6d1e76957
Steva0 <michele.stevanin@gmail.com>
2025-05-28 14:12:13 +0200

    Mmmmodifche Database e Deserializer. Aggiunti i commenti a quello che teoriacamente fa ogni metodo

--- src/Memory/Database.cpp
+++ src/Memory/Database.cpp
@@ -3,11 +3,18 @@
 #include <QFile>
 #include <memory>
 
-#include "Deserializer.h"
 namespace memory {
 Database::~Database() {
   close(false); // di default non salvare eventuali cambiamenti
 }
+Database::Database(const QString &path) : file_(path) {
+  if (!file_.open(QIODevice::ReadWrite | QIODevice::Text)) {
+    // errore apertura file
+    return;
+  }
+  // media_container_.addMedia(Deserializer::deserialize(file_));
+}
+
 bool Database::open(const QString &path) {
   QFile file(path);
   if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
@@ -38,8 +45,23 @@ bool Database::save() {
 }
 
 std::vector<const media::Media*> Database::filterMedia(
-    const media::Media &media_as_filter) {
+    const media::Media &media_as_filter) const {
   return media_container_.filter(media_as_filter);
 }
 
+void Database::addMedia(const media::Media &media) {
+    media_container_.addMedia(media);
+}
+
+void Database::removeMedia(const media::Media &media) {
+  media_container_.removeMedia(media);
+}
+
+void Database::clear(){
+  media_container_.clear();
+}
+
+std::vector<const media::Media*> Database::getAll() const {
+  return media_container_.getAll();
+}
 }  // namespace memory
--- src/Memory/Database.h
+++ src/Memory/Database.h
@@ -11,14 +11,31 @@ class Database {
   MediaContainer media_container_;
   QSaveFile file_;
 
+  class Deserializer {
+    private:
+     static std::vector<media::Media> fromJson(QFile &file);
+     static std::vector<media::Media> fromXML(QFile &file);
+    
+   public:
+    static std::vector<media::Media> deserialize(QFile &file);
+  };
+
  public:
+  Database(){};   //costruttore vuoto
+  Database(const QString &path);  //costruttore partendo da un file
+
   ~Database();
 
-  bool open(const QString &path);
-  bool close(bool save);
-  bool save();
+  bool open(const QString &path);   //fa un clear e apre il file passato e lo assegna all'attributo file_
+  bool close(bool save);  //se save == true, salva il database su file_ altrimenti fa solo un clear
+  bool save();      //serializza il database su file_
 
-  std::vector<const media::Media*> filterMedia(const media::Media &);
-};
+  void addMedia(const media::Media &media); //aggiunge un media al database
+  void removeMedia(const media::Media &media);  //rimuove un media dal database
+  std::vector<const media::Media*> getAll() const;  //restituisce tutti i media del database
+  void clear(); //svuota il database
+
+  std::vector<const media::Media*> filterMedia(const media::Media &) const; //filtra i media del database in base al filtro passato come parametro
+}; 
 }  // namespace memory
 #endif
