575744521a94d5f9178750f3c44f3f6b7c3b0d98
Steva0 <michele.stevanin@gmail.com>
2025-05-29 15:24:01 +0200

    Modificato xml visitor per non salvare nel file i campi vuoti

--- src/Memory/MediaXMLVisitor.cpp
+++ src/Memory/MediaXMLVisitor.cpp
@@ -37,87 +37,121 @@ void MediaXMLVisitor::visit(const media::Media &media) {
   QDomElement el;
 
   base.appendChild(elementFrom("Title", media.getTitle()));
-  base.appendChild(elementFrom("Release", media.getRelease()));
-  base.appendChild(elementFrom("Language", media.getLanguage()));
-  if (media.isFavourite()) base.appendChild(elementFrom("Favourite"));
 
-  el = document_.createElement("Genres");
-  for (const std::string &genre : media.getGenres()) {
-    el.appendChild(elementFrom("Genre", genre));
+  if (media.getRelease() != std::numeric_limits<int>::min())
+    base.appendChild(elementFrom("Release", media.getRelease()));
+  if (media.getLanguage() != "")
+    base.appendChild(elementFrom("Language", media.getLanguage()));
+  if (media.isFavourite())
+    base.appendChild(elementFrom("Favourite"));
+
+  if (!media.getGenres().empty()) {
+    el = document_.createElement("Genres");
+    for (const std::string &genre : media.getGenres()) {
+      el.appendChild(elementFrom("Genre", genre));
+    }
+    base.appendChild(el);
   }
-  base.appendChild(el);
 
-  base.appendChild(elementFrom("ImagePath", media.getImgPath()));
-  base.appendChild(elementFrom("Notes", media.getNotes()));
+  if (media.getImgPath() != "")
+    base.appendChild(elementFrom("ImagePath", media.getImgPath()));
+  if (media.getNotes() != "")
+    base.appendChild(elementFrom("Notes", media.getNotes()));
+
   document_.appendChild(base);
 }
+
 void MediaXMLVisitor::visit(const media::Album &album) {
   visit(static_cast<const media::Media &>(album));
   document_.childNodes().at(0).toElement().setTagName("Album");
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Band", album.getBand()));
-  QDomElement el = document_.createElement("BandMembers");
-  for (const std::string &member : album.getBandMembers()) {
-    el.appendChild(elementFrom("Member", member));
+  if (album.getBand() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Band", album.getBand()));
+  if (!album.getBandMembers().empty()) {
+    QDomElement el = document_.createElement("BandMembers");
+    for (const std::string &member : album.getBandMembers()) {
+      el.appendChild(elementFrom("Member", member));
+    }
+    document_.childNodes().at(0).toElement().appendChild(el);
   }
-  document_.childNodes().at(0).toElement().appendChild(el);
-  el = document_.createElement("Songs");
-  for (const std::string &song : album.getSongs()) {
-    el.appendChild(elementFrom("Song", song));
+  if (!album.getSongs().empty()) {
+    QDomElement el = document_.createElement("Songs");
+    for (const std::string &song : album.getSongs()) {
+      el.appendChild(elementFrom("Song", song));
+    }
+    document_.childNodes().at(0).toElement().appendChild(el);
   }
-  document_.childNodes().at(0).toElement().appendChild(el);
 }
+
 void MediaXMLVisitor::visit(const media::Movie &movie) {
   visit(static_cast<const media::Media &>(movie));
   document_.childNodes().at(0).toElement().setTagName("Movie");
-  QDomElement el = document_.createElement("Cast");
-  for (const std::string &act : movie.getCast()) {
-    el.appendChild(elementFrom("Act", act));
+  if (!movie.getCast().empty()) {
+    QDomElement el = document_.createElement("Cast");
+    for (const std::string &act : movie.getCast()) {
+      el.appendChild(elementFrom("Act", act));
+    }
+    document_.childNodes().at(0).toElement().appendChild(el);
   }
-  document_.childNodes().at(0).toElement().appendChild(el);
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Length", movie.getLength()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Universe", movie.getUniverse()));
+  if (movie.getLength() != std::numeric_limits<int>::min())
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Length", movie.getLength()));
+  if (movie.getUniverse() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Universe", movie.getUniverse()));
 }
+
 void MediaXMLVisitor::visit(const media::Series &series) {
   visit(static_cast<const media::Movie &>(series));
   document_.childNodes().at(0).toElement().setTagName("Series");
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Episodes", series.getEpisodes()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Seasons", series.getSeasons()));
+  if (series.getEpisodes() != std::numeric_limits<int>::min())
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Episodes", series.getEpisodes()));
+  if (series.getSeasons() != std::numeric_limits<int>::min())
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Seasons", series.getSeasons()));
   if (series.hasEnded())
     document_.childNodes().at(0).toElement().appendChild(elementFrom("Ended"));
 }
+
 void MediaXMLVisitor::visit(const media::Novel &novel) {
   visit(static_cast<const media::Media &>(novel));
   document_.childNodes().at(0).toElement().setTagName("Novel");
 
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Author", novel.getAuthor()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Publisher", novel.getPublisher()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Pages", novel.getPages()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Series", novel.getSeries()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("ISBN", novel.getIsbn()));
+  if (novel.getAuthor() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Author", novel.getAuthor()));
+  if (novel.getPublisher() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Publisher", novel.getPublisher()));
+  if (novel.getPages() != std::numeric_limits<int>::min())
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Pages", novel.getPages()));
+  if (novel.getSeries() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Series", novel.getSeries()));
+  if (novel.getIsbn() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("ISBN", novel.getIsbn()));
 }
+
 void MediaXMLVisitor::visit(const media::AudioBook &audioBook) {
   visit(static_cast<const media::Novel &>(audioBook));
   document_.childNodes().at(0).toElement().setTagName("AudioBook");
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Narrator", audioBook.getNarrator()));
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Service", audioBook.getStreamingService()));
+  if (audioBook.getNarrator() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Narrator", audioBook.getNarrator()));
+  if (audioBook.getStreamingService() != "")
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Service", audioBook.getStreamingService()));
 }
+
 void MediaXMLVisitor::visit(const media::Ebook &ebook) {
   visit(static_cast<const media::Novel &>(ebook));
   document_.childNodes().at(0).toElement().setTagName("Ebook");
-  document_.childNodes().at(0).toElement().appendChild(
-      elementFrom("Bytes", ebook.getFileSizeBytes()));
+  if (ebook.getFileSizeBytes() != std::numeric_limits<int>::min())
+    document_.childNodes().at(0).toElement().appendChild(
+        elementFrom("Bytes", ebook.getFileSizeBytes()));
   if (ebook.hasDrm())
     document_.childNodes().at(0).toElement().appendChild(elementFrom("DRM"));
 }
