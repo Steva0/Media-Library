748d94efba9b3c63b3c6cc89681dc70b4a540484
b0dyless <almor.social@proton.me>
2025-06-25 21:03:14 +0200

    Cancellati file vecchi

--- tmp/ProfileButton.cpp
+++
@@ -1,9 +0,0 @@
-#include "ProfileButton.h"
-
-ProfileButton::ProfileButton(const QString &profile_name, QWidget *parent) : QPushButton(profile_name, parent) {
- 
-}
-
-int ProfileButton::heightForWidth(int w) const {
-  return w;
-}
