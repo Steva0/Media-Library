720bac15fc07dd6483843c1ec900f91a88d634b1
b0dyless <almor.social@proton.me>
2025-06-28 16:39:31 +0200

    Ricerca semplice risultati immediati

--- src/GUI/Search/InputBar.cpp
+++ src/GUI/Search/InputBar.cpp
@@ -6,6 +6,7 @@ const int InputBar::kTimerDuration = 500;
 InputBar::InputBar(QWidget *parent) : QLineEdit(parent), timer_(new QTimer(this)) {
   timer_->setSingleShot(true);
   timer_->setInterval(kTimerDuration);
+  timer_->start(kTimerDuration);
 
   connect(this, &QLineEdit::textEdited, timer_, [this]() { timer_->start(kTimerDuration); });
   connect(timer_, &QTimer::timeout, this, [this]() { emit timedEdit(text()); });
