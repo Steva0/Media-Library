199b8cb2f552c00435c5aaac67ab771c269180e4
Okonigiri <207894241+Okonigiri@users.noreply.github.com>
2025-08-28 18:16:02 +0200

    Idea commento StreamingServices.h

--- src/GUI/StreamingServices.h
+++ src/GUI/StreamingServices.h
@@ -1,6 +1,11 @@
 #ifndef STREAMINGSERVICES_H
 #define STREAMINGSERVICES_H
 
+// L'implementazione logica non offre una lista di servizi streaming per audiolibri.
+// Abbiamo deciso di definirla all'interno dell'interfaccia grafica, ritenendo che
+// ciò non violi la separazione tra implementazione logica e grafica,
+// immaginandola una funzionalità introdotta da un utilizzatore della libreria
+// (= l'implementazione logica).
 #include <QStringList>
 
 namespace gui {
